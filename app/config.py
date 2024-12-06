0a943fefe0b5f50593a7f6207494c02b86f497a8
NikitaKharzheev <kharzheev1999@mail.ru>
2024-12-06 03:30:15 +0300

    Fixed config and added get list function to s3_service file

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 import os
 
 S3_ENDPOINT_URL = os.getenv("S3_ENDPOINT_URL", "http://localhost:9000")
-S3_ACCESS_KEY = os.getenv("S3_ACCESS_KEY", "minio123")
-S3_SECRET_KEY = os.getenv("S3_SECRET_KEY", "minio123")
+S3_ACCESS_KEY = os.getenv("S3_ACCESS_KEY", "minioadmin")
+S3_SECRET_KEY = os.getenv("S3_SECRET_KEY", "minioadmin")
 S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME", "mlops-bucket")
--- app/s3_service.py
+++ app/s3_service.py
@@ -3,11 +3,19 @@ from botocore.exceptions import NoCredentialsError
 from config import S3_ENDPOINT_URL, S3_ACCESS_KEY, S3_SECRET_KEY, S3_BUCKET_NAME
 
 s3_client = boto3.client(
-    's3',
+    "s3",
     endpoint_url=S3_ENDPOINT_URL,
     aws_access_key_id=S3_ACCESS_KEY,
-    aws_secret_access_key=S3_SECRET_KEY
+    aws_secret_access_key=S3_SECRET_KEY,
 )
+
+
+def create_bucket(bucket_name: str = S3_BUCKET_NAME):
+    try:
+        s3_client.create_bucket(Bucket=bucket_name)
+    except Exception as e:
+        raise ValueError(f"Error: {e}")
+
 
 def upload_to_s3(file_path: str, s3_key: str):
     try:
@@ -17,8 +25,21 @@ def upload_to_s3(file_path: str, s3_key: str):
     except NoCredentialsError:
         raise ValueError("Invalid S3 credentials")
 
+
 def download_from_s3(s3_key: str, download_path: str):
     try:
         s3_client.download_file(S3_BUCKET_NAME, s3_key, download_path)
     except Exception as e:
         raise ValueError(f"Failed to download file: {e}")
+
+
+def get_list_from_bucket(folder_name: str):
+    bucket_name = S3_BUCKET_NAME
+    prefix = folder_name
+
+    response = s3_client.list_objects_v2(Bucket=bucket_name, Prefix=prefix)
+    trained_models = [
+        obj["Key"][obj["Key"].find("/") + 1 : obj["Key"].rfind(".")]
+        for obj in response["Contents"]
+    ]
+    return trained_models
